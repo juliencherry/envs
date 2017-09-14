56e9a21b5b25ca0f449511de31fd2f5d4eb33518
Julien Cherry <jcherry@pivotal.io>
2017-09-14 10:41:00 -0400

    Add `cf-add-target` and `cf-target` commands
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- envs_test.go
+++ envs_test.go
@@ -1,6 +1,8 @@
 package main_test
 
 import (
+	"io/ioutil"
+	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
@@ -10,22 +12,139 @@ import (
 )
 
 var _ = Describe("Envs", func() {
+	var (
+		pathToCLI        string
+		envStateFilepath string
+	)
 
-	var pathToCLI string
-
-	BeforeSuite(func() {
-		var err error
-		pathToCLI, err = Build("github.com/juliencherry/envs")
+	BeforeEach(func() {
+		envStateFile, err := ioutil.TempFile("", "env-state")
+		Expect(err).NotTo(HaveOccurred())
+		envStateFilepath = envStateFile.Name()
+		pathToCLI, err = Build("github.com/juliencherry/envs", "-ldflags", "-X main.envStateFilepath="+envStateFilepath)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	Context("when no CF targets have been added", func() {
-		It("outputs that no targets are available", func() {
-			command := exec.Command(pathToCLI, "cf-targets")
-			session, err := Start(command, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
+	AfterEach(func() {
+		os.Remove(envStateFilepath)
+		CleanupBuildArtifacts()
+	})
+
+	Context("when an invalid command is used", func() {
+		It("indicates that it was an invalid command", func() {
+			session := runCommand(pathToCLI, "an-invalid-command")
+			Eventually(session).Should(Exit(1))
+			Expect(session).To(Say("Invalid command"))
+		})
+	})
+
+	Context("when no command is used", func() {
+		It("prints an error message", func() {
+			session := runCommand(pathToCLI)
+			Eventually(session).Should(Exit(1))
+			Expect(session).To(Say("Invalid command"))
+		})
+	})
+
+	Describe("cf-targets", func() {
+		Context("when no CF targets have been added", func() {
+			It("outputs that no targets are available", func() {
+				session := runCommand(pathToCLI, "cf-targets")
+				Eventually(session).Should(Exit(0))
+				Expect(session).To(Say("No targets available"))
+			})
+		})
+
+		Context("when the envs file does not exist", func() {
+			BeforeEach(func() {
+				os.Remove(envStateFilepath)
+			})
+
+			It("outputs that no targets are available", func() {
+				session := runCommand(pathToCLI, "cf-targets")
+				Eventually(session).Should(Exit(0))
+				Expect(session).To(Say("No targets available"))
+			})
+		})
+
+		Context("when the command errors", func() {
+			BeforeEach(func() {
+				os.Remove(envStateFilepath)
+				os.Mkdir(envStateFilepath, 0700)
+			})
+
+			AfterEach(func() {
+				os.RemoveAll(envStateFilepath)
+			})
+
+			It("prints an error message", func() {
+				session := runCommand(pathToCLI, "cf-targets")
+				Eventually(session).Should(Exit(1))
+				Expect(session).To(Say("Failed to list targets: "))
+			})
+		})
+	})
+
+	Describe("cf-add-target", func() {
+		It("allows multiple targets to be added", func() {
+			someEnv := "some-env"
+			session := runCommand(pathToCLI, "cf-add-target", someEnv)
 			Eventually(session).Should(Exit(0))
-			Expect(session).To(Say("No targets available"))
+
+			someOtherEnv := "some-other-env"
+			session = runCommand(pathToCLI, "cf-add-target", someOtherEnv)
+			Eventually(session).Should(Exit(0))
+
+			session = runCommand(pathToCLI, "cf-targets")
+			Eventually(session).Should(Exit(0))
+			Expect(session).To(Say(someEnv + "\n"))
+			Expect(session).To(Say(someOtherEnv + "\n"))
+		})
+
+		It("does not add duplicate targets", func() {
+			someEnv := "some-env"
+			session := runCommand(pathToCLI, "cf-add-target", someEnv)
+			Eventually(session).Should(Exit(0))
+
+			session = runCommand(pathToCLI, "cf-add-target", someEnv)
+			Eventually(session).Should(Exit(1))
+			Expect(session).To(Say("target “" + someEnv + "” already exists"))
+
+			session = runCommand(pathToCLI, "cf-targets")
+			Eventually(session).Should(Exit(0))
+			Expect(string(session.Out.Contents())).To(Equal(someEnv + "\n"))
+		})
+
+		Context("when no target is given", func() {
+			It("indicates that a target must be specified", func() {
+				session := runCommand(pathToCLI, "cf-add-target")
+				Eventually(session).Should(Exit(1))
+				Expect(session).To(Say("Must specify target to add"))
+			})
+		})
+
+		Context("when the command errors", func() {
+			BeforeEach(func() {
+				os.Remove(envStateFilepath)
+				os.Mkdir(envStateFilepath, 0700)
+			})
+
+			AfterEach(func() {
+				os.RemoveAll(envStateFilepath)
+			})
+
+			It("prints an error message", func() {
+				session := runCommand(pathToCLI, "cf-add-target", "some-env")
+				Eventually(session).Should(Exit(1))
+				Expect(session).To(Say("Failed to add the target: "))
+			})
 		})
 	})
 })
+
+func runCommand(bin string, args ...string) *Session {
+	command := exec.Command(bin, args...)
+	session, err := Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	return session
+}
--- main.go
+++ main.go
@@ -1,7 +1,102 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var envStateFilepath string
 
 func main() {
-	fmt.Println("No targets available")
+	if envStateFilepath == "" {
+		envStateFilepath = filepath.Join(os.Getenv("HOME"), ".envs")
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Invalid command")
+		os.Exit(1)
+	}
+	command := os.Args[1]
+
+	var commandErr error
+	switch command {
+	case "cf-add-target":
+
+		if len(os.Args) < 3 {
+			fmt.Println("Must specify target to add")
+			os.Exit(1)
+		}
+
+		if err := cfAddTarget(os.Args[2]); err != nil {
+			commandErr = fmt.Errorf("Failed to add the target: %s", err.Error())
+		}
+	case "cf-targets":
+		if err := cfTargets(); err != nil {
+			commandErr = fmt.Errorf("Failed to list targets: %s", err.Error())
+		}
+	default:
+		commandErr = errors.New("Invalid command")
+	}
+
+	if commandErr != nil {
+		fmt.Println(commandErr.Error())
+		os.Exit(1)
+	}
+}
+
+func cfAddTarget(targetName string) error {
+
+	contents, err := getFileContents(envStateFilepath)
+	if err != nil {
+		return err
+	}
+
+	lines := strings.Split(contents, "\n")
+	for _, line := range lines {
+		if line == targetName {
+			return fmt.Errorf("target “%s” already exists", targetName)
+		}
+	}
+
+	file, err := os.OpenFile(envStateFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(targetName + "\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func cfTargets() error {
+	envState, err := getFileContents(envStateFilepath)
+	if err != nil {
+		return err
+	}
+
+	if envState == "" {
+		fmt.Println("No targets available")
+	} else {
+		fmt.Print(envState)
+	}
+
+	return nil
+}
+
+func getFileContents(path string) (string, error) {
+	contents, err := ioutil.ReadFile(path)
+
+	if err != nil && !os.IsNotExist(err) {
+		return "", err
+	}
+
+	return string(contents), nil
 }
